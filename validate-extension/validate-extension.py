29bf2b888693feab0e9978e2ad72c4bb95d83a22
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-09-26 11:05:34 +0200

    Great improve python script to validate an extension file

--- validate-extension/validate-extension.py
+++ validate-extension/validate-extension.py
@@ -5,18 +5,87 @@ import json
 import jsonschema
 import jsonref
 
-schema = '../../schema/v09/extensions/extension.schema.json'
-js = json.loads(open(schema).read())
+metaschemapath = '/Users/hugo/projects/nl3d/schemas/extensions/extension.schema.json'
+jmeta = json.loads(open(metaschemapath).read())
 
-dataset = '../../schema/v09/extensions/misc.json'
-j = json.loads(open(dataset).read())
+schemapath = '/Users/hugo/projects/nl3d/schemas/extensions/nl_3d.json'
+js = json.loads(open(schemapath).read())
 
-try:
-    jsonschema.validate(j, js)
-    print ("All good!")
-except jsonschema.ValidationError as e:
-    print ("ERROR:   ", e.message)
-except jsonschema.SchemaError as e:
-    print ("ERROR:   ", e)
+def main():
+    #-- 1. validation against the extension schema ("meta")
+    try:
+        jsonschema.validate(js, jmeta)
+    except jsonschema.ValidationError as e:
+        print ("ERROR:   ", e.message)
+        bye(False)
+    except jsonschema.SchemaError as e:
+        print ("ERROR:   ", e)
+        bye(False)
+
+    folder_schemas = os.path.abspath(os.path.dirname(schemapath))
+    # base_uri = os.path.join(folder_schemas, "extensions")
+    base_uri = os.path.abspath(folder_schemas)
+
+    #-- 2. extraCityObjects
+    if "extraCityObjects" in js:
+        for nco in js["extraCityObjects"]:
+            jtmp = {}
+            jtmp["$schema"] = "http://json-schema.org/draft-07/schema#"
+            jtmp["type"] = "object"
+            jtmp["$ref"] = "file://%s#/extraCityObjects/%s" % (schemapath, nco)
+            jsotf = jsonref.loads(json.dumps(jtmp), jsonschema=True, base_uri=base_uri)
+            try:
+                jsonschema.Draft7Validator.check_schema(jsotf)
+            except jsonschema.exceptions.SchemaError as e:
+                print("ERROR:")
+                print(e)
+                bye(False)
+            except jsonref.JsonRefError as e:
+                print("ERROR:")
+                print(e)
+                bye(False)
+
+    #-- 3. extraRootProperties
+    if "extraRootProperties" in js:
+        for nrp in js["extraRootProperties"]:
+            jtmp = {}
+            jtmp["$schema"] = "http://json-schema.org/draft-07/schema#"
+            jtmp["type"] = "object"
+            jtmp["$ref"] = "file://%s#/extraRootProperties/%s" % (schemapath, nrp)
+            jsotf = jsonref.loads(json.dumps(jtmp), jsonschema=True, base_uri=base_uri)
+            try:
+                jsonschema.Draft7Validator.check_schema(jsotf)
+            except  jsonschema.exceptions.SchemaError as e:
+                print("ERROR:")
+                print(e)
+                bye(False)
+
+    #-- 4. extraAttributes
+    if "extraAttributes" in js:
+        for thetype in js["extraAttributes"]:
+            for ea in js["extraAttributes"][thetype]:
+                jtmp = {}
+                jtmp["$schema"] = "http://json-schema.org/draft-07/schema#"
+                jtmp["type"] = "object"
+                jtmp["$ref"] = "file://%s#/extraAttributes/%s/%s" % (schemapath, thetype, ea)
+                jsotf = jsonref.loads(json.dumps(jtmp), jsonschema=True, base_uri=base_uri)
+                try:
+                    jsonschema.Draft7Validator.check_schema(jsotf)
+                except  jsonschema.exceptions.SchemaError as e:
+                    print("ERROR:")
+                    print(e)
+                    bye(False)
+
+    bye(True)
 
 
+def bye(isValid):
+    if isValid == True:
+        print("\n--> CityJSON Extension is VALID")
+    else:
+        print("\n--> CityJSON Extension is INVALID :(")
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
